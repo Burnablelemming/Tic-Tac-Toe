7563aac569e2d0a37d9f8a5e06370ab93deef051
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-02-15 17:10:32 -0600

    Created empty functions for future use. Created getPlayerMove method to get position on board.

--- TicTacToe/Models/GameManager.cs
+++ TicTacToe/Models/GameManager.cs
@@ -12,7 +12,7 @@ namespace TicTacToe.Models
         private int _moves;
 
         private Result _gameResult;
-        public GameManager() 
+        public GameManager()
         {
             _moves = 0;
             _gameResult = Enums.Result.Undecided;
@@ -24,6 +24,44 @@ namespace TicTacToe.Models
         //    _gameResult = result;
         //} 
 
+        public void StartGame(Player player1, Player player2)
+        {
+            //Board board = new Board();
+            // while (moves < 9 and gameResult == Undecided)
+            //makeMove(player, board)
+        }
 
+        public int MakeMove(Player player)
+        {
+            
+        }
+
+        public int getPlayerMove()
+        {
+            int position = -1; //Before user input
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                try
+                {
+                    Console.Write("Enter a number between 1 and 9: ");
+                    position = int.Parse(Console.ReadLine());
+
+                    if (position < 1 || position > 9) // Input out of range
+                    {
+                        throw new Exception();  
+                    }
+
+                    validInput = true;  // Valid input
+                }
+                catch (Exception)  // Catches any invalid inputs
+                {
+                    Console.WriteLine("Invalid input. Enter a number between 1-9.");
+                }
+
+            }
+            return position;
+        }
     }
 }
