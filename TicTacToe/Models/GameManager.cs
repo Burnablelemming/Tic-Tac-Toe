94c53103d9fa9841183baba266ec81f7fee1f13a
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-02-15 23:23:34 -0600

    Updated getPlayerMove method to check a new field usedPositions which is a list of positions filled on the board. Added switch case to the Make move method that assigns the state of the player to the square.

--- TicTacToe/Models/GameManager.cs
+++ TicTacToe/Models/GameManager.cs
@@ -12,6 +12,8 @@ namespace TicTacToe.Models
         private int _moves;
 
         private Result _gameResult;
+
+        private List<int> __usedPositions = new List<int>; //Keeps track of positions filled by storing them in the array.
         public GameManager()
         {
             _moves = 0;
@@ -26,17 +28,71 @@ namespace TicTacToe.Models
 
         public void StartGame(Player player1, Player player2)
         {
-            //Board board = new Board();
-            // while (moves < 9 and gameResult == Undecided)
-            //makeMove(player, board)
-        }
-
-        public int MakeMove(Player player)
-        {
+            Board gameBoard = new Board();
+            while (this._moves < 9 && this._gameResult == Enums.Result.Undecided) 
+            {
+                Console.WriteLine("Player 1 Makes move");
+                MakeMove(player1, gameBoard);
+                MakeMove(player2 , gameBoard);
+            }
             
         }
 
-        public int getPlayerMove()
+        public void MakeMove(Player player, Board gameBoard)
+        {
+            int position = getPlayerMove(gameBoard);
+
+            switch (position)
+            {
+                case 1:
+                    gameBoard.board[0][0].SquareState = player.PlayerChoice;
+                    _usedPositions[0] = position;
+                    _moves++;
+                    break;
+                case 2:
+                    gameBoard.board[0][1].SquareState = player.PlayerChoice;
+                    __usedPositions[1] = position;
+                    _moves++;
+                    break;
+                case 3:
+                    gameBoard.board[0][2].SquareState = player.PlayerChoice;
+                    __usedPositions[2] = position;
+                    _moves++;
+                    break;
+                case 4:
+                    gameBoard.board[1][0].SquareState = player.PlayerChoice;
+                    _usedPositions[3] = position;
+                    _moves++;
+                    break;
+                case 5:
+                    gameBoard.board[1][1].SquareState = player.PlayerChoice;
+                    _usedPositions[4] = position;
+                    _moves++;
+                    break;
+                case 6:
+                    gameBoard.board[1][2].SquareState = player.PlayerChoice;
+                    _usedPositions[5] = position;
+                    _moves++;
+                    break;
+                case 7:
+                    gameBoard.board[2][0].SquareState = player.PlayerChoice;
+                    _usedPositions[6] = position;
+                    _moves++;
+                    break;
+                case 8:
+                    gameBoard.board[2][1].SquareState = player.PlayerChoice;
+                    _usedPositions[7] = position;
+                    _moves++;
+                    break;
+                case 9:
+                    gameBoard.board[2][2].SquareState = player.PlayerChoice;
+                    _usedPositions[8] = position;
+                    _moves++;
+                    break;
+            }
+        }
+
+        public int getPlayerMove(Board gameBoard)
         {
             int position = -1; //Before user input
             bool validInput = false;
@@ -51,6 +107,10 @@ namespace TicTacToe.Models
                     if (position < 1 || position > 9) // Input out of range
                     {
                         throw new Exception();  
+                    }
+                    else if (this._usedPositions.Contains(position)) 
+                    {
+                        Console.WriteLine("Invalid Input. Position already filled on board.");
                     }
 
                     validInput = true;  // Valid input
